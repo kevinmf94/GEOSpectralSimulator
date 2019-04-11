1a0e2ab1ef7ef9f3eacf4d1023b89d6ccecfd44b
Kevin Martín <kevinmf94@gmail.com>
2019-04-11 19:16:54 +0200

    Monitoring report I

---
+++ Unreal/GEOSimulator/Source/GEOSimulator/CameraHUD.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "VehiclePawn.h"
+#include "CameraHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEOSIMULATOR_API ACameraHUD : public AHUD
+{
+	GENERATED_BODY()
+protected:
+	virtual void BeginPlay() override;
+public:
+	virtual void Tick(float DeltaSeconds) override;
+	virtual void DrawHUD() override;
+	void SetTexture(UTexture* texture);
+
+	AVehiclePawn* player;
+	UCameraComponent* camera;
+	
+	UPROPERTY()
+	UTexture* texture = nullptr;
+
+};
