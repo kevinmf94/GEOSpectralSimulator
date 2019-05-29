62359d882ae7a13dc0a7fd302444606dcfa1832d
Kevin Martín <kevinmf94@gmail.com>
2019-05-30 00:17:37 +0200

    Generate map json with the files and coordinates

--- GEOTool/src/GEOTool.py
+++ GEOTool/src/GEOTool.py
@@ -1,8 +1,11 @@
 import sys
+import json
 from geotool import *
 
 if __name__ == '__main__':
 	config = GEOConfig(sys.argv[1])
+
+	mapData = []
 
 	for h in range(config.chunks[0]):
 		for w in range(config.chunks[1]):
@@ -16,12 +19,24 @@ if __name__ == '__main__':
 			wcsData = wcsClient.send_request()
 			wcsData.save(filename, config.formatWcs, config.meshStep)
 			WCSSaver.generate_geojson(filename, wcsData)
-
+		
+			textures = {}
 			for wms_request in config.wmsRequests:
-				filename_tex = filename+"_"+wms_request["layers"]
+				filename_tex = filename+"_"+wms_request["name"]
 
 				wmsClient = WMSClient(wms_request["url"], config.coordinates, config.bboxSize,
 				                      config.textureSize, wms_request["layers"], offset, config.cellsize)
 
 				data = wmsClient.send_request()
 				WMSSaverJPG().save(filename_tex, data)
+				textures[wms_request["name"]] = filename_tex+"."+wms_request['format']
+
+			mapData.append({
+				"file": filename+"."+config.formatWcs,
+				"x": wcsData.xllcorner,
+				"y": wcsData.yllcorner,
+				"textures": textures
+			})
+
+	with open(config.outputWcs+'.json', 'w') as outfile:
+		json.dump(mapData, outfile, indent=4)
